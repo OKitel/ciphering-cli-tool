274d421a31f0dc227b394b1c7d40810303c576eb
OKitel <kitelolga@gmail.com>
2021-11-11 23:39:47 +0100

    feat: add flags variables for input/output arguments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const { stderr, exit } = process;
 let flags = process.argv;
 const possibleCipher = ["C0", "C1", "R0", "R1", "A", "A0", "A1"];
-const possibleOptions = ["-c", "--config", "-i", "input", "-o", "--output"];
+const possibleOptions = ["-c", "--config", "-i", "--input", "-o", "--output"];
+const includesInputOption = flags.includes("-i") || flags.includes("--input");
+const includesOutputOption = flags.includes("-o") || flags.includes("--output");
+console.log(includesInputOption, includesOutputOption);
 
 const checkCipherSequence = (config) => {
   let isOk = true;
