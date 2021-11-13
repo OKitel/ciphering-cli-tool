e8a41b0688b9dfbc3192ebdcef5aae4e041dfadd
OKitel <kitelolga@gmail.com>
2021-11-13 14:19:40 +0100

    feat: adc transform stream classes for rot8 and atbash ciphers

--- src/streams.js
+++ src/streams.js
@@ -1,7 +1,7 @@
 import { Transform } from "stream";
 import { ceasarEncode, ceasarDecode } from "./ciphers/ceasar.js";
-// import { rot8Encode, rot8Decode } from "./ciphers/rot-8";
-// import { atbashEncode, atbashDecode } from "./ciphers/atbash";
+import { rot8Encode, rot8Decode } from "./ciphers/rot-8.js";
+import { atbashEncode } from "./ciphers/atbash.js";
 
 export const addTransformStream = () => {
   //TODO
@@ -38,6 +38,60 @@ export class CeasarDecoder extends Transform {
     }
 
     const decodedMessage = ceasarDecode(chunkStringified);
+
+    callback(null, decodedMessage);
+  }
+}
+
+export class Rot8Encoder extends Transform {
+  constructor(options) {
+    super(options);
+  }
+
+  _transform(chunk, encoding, callback) {
+    const chunkStringified = chunk.toString();
+
+    if (chunkStringified === "\u0003") {
+      process.exit();
+    }
+
+    const decodedMessage = rot8Encode(chunkStringified);
+
+    callback(null, decodedMessage);
+  }
+}
+
+export class Rot8Decoder extends Transform {
+  constructor(options) {
+    super(options);
+  }
+
+  _transform(chunk, encoding, callback) {
+    const chunkStringified = chunk.toString();
+
+    if (chunkStringified === "\u0003") {
+      process.exit();
+    }
+
+    const decodedMessage = rot8Decode(chunkStringified);
+
+    callback(null, decodedMessage);
+  }
+}
+
+export class AtbashEncoderDecoder extends Transform {
+  constructor(options) {
+    super(options);
+  }
+
+  _transform(chunk, encoding, callback) {
+    const chunkStringified = chunk.toString();
+
+    if (chunkStringified === "\u0003") {
+      process.exit();
+    }
+
+    const decodedMessage = atbashEncode(chunkStringified);
 
     callback(null, decodedMessage);
   }
