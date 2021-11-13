c60d7104251e1acb9d68dab7b91876eb85c183c4
OKitel <kitelolga@gmail.com>
2021-11-13 13:38:09 +0100

    feat: add custom transform stream for Ceasar cipher

--- src/streams.js
+++ src/streams.js
@@ -1,5 +1,44 @@
-// import { Transform } from "stream";
+import { Transform } from "stream";
+import { ceasarEncode, ceasarDecode } from "./ciphers/ceasar.js";
+// import { rot8Encode, rot8Decode } from "./ciphers/rot-8";
+// import { atbashEncode, atbashDecode } from "./ciphers/atbash";
 
 export const addTransformStream = () => {
   //TODO
 };
+
+export class CeasarEncoder extends Transform {
+  constructor(options) {
+    super(options);
+  }
+
+  _transform(chunk, encoding, callback) {
+    const chunkStringified = chunk.toString();
+
+    if (chunkStringified === "\u0003") {
+      process.exit();
+    }
+
+    const encodedMessage = ceasarEncode(chunkStringified);
+
+    callback(null, encodedMessage);
+  }
+}
+
+export class CeasarDecoder extends Transform {
+  constructor(options) {
+    super(options);
+  }
+
+  _transform(chunk, encoding, callback) {
+    const chunkStringified = chunk.toString();
+
+    if (chunkStringified === "\u0003") {
+      process.exit();
+    }
+
+    const decodedMessage = ceasarDecode(chunkStringified);
+
+    callback(null, decodedMessage);
+  }
+}
