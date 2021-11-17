6ecc8452d5ed869b00207558ddb648633398f0d0
OKitel <kitelolga@gmail.com>
2021-11-17 23:24:53 +0100

    feat: add test for ceasar decoding function

--- tests/ceasar.test.js
+++ tests/ceasar.test.js
@@ -1,8 +1,15 @@
-import { ceasarEncode } from "../src/ciphers/ceasar.js";
+import { ceasarEncode, ceasarDecode } from "../src/ciphers/ceasar.js";
 
 test("should encode message using Ceasar cipher", () => {
   const message = 'This is secret. Message about "_" symbol!';
   expect(ceasarEncode(message)).toEqual(
     'Uijt jt tfdsfu. Nfttbhf bcpvu "_" tzncpm!'
+  );
+});
+
+test("should decode message using Ceasar cipher", () => {
+  const message = 'Uijt jt tfdsfu. Nfttbhf bcpvu "_" tzncpm!';
+  expect(ceasarDecode(message)).toEqual(
+    'This is secret. Message about "_" symbol!'
   );
 });
