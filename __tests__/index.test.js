06d2b7781008eb06bde8185b8f485c55439898c3
OKitel <kitelolga@gmail.com>
2021-11-21 16:02:07 +0100

    feat: add tests for usage examples from 1st task

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -33,6 +33,130 @@ describe("testing all cli with child process", () => {
     });
   });
 
+  test("should return correct answer fro usage example #1", (done) => {
+    const INPUT_TO_CLI = [
+      "./src/index.js",
+      "-c",
+      "C1-C1-R0-A",
+      "-i",
+      "./src/input.txt",
+    ];
+    const EXPECTED_OUTPUT = 'Myxn xn nbdobm. Tbnnfzb ferlm "_" nhteru!';
+    const cp = spawn("node", INPUT_TO_CLI);
+
+    let res = "";
+
+    cp.stdout.on("data", (chunk) => {
+      res += chunk.toString();
+    });
+
+    cp.stderr.on("data", (chunk) => {
+      res += chunk.toString();
+    });
+
+    cp.stdout.on("end", () => {
+      try {
+        expect(res).toBe(EXPECTED_OUTPUT);
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
+
+  test("should return correct answer fro usage example #2", (done) => {
+    const INPUT_TO_CLI = [
+      "./src/index.js",
+      "-c",
+      "C1-C0-A-R1-R0-A-R0-R0-C1-A",
+      "-i",
+      "./src/input.txt",
+    ];
+    const EXPECTED_OUTPUT = 'Vhgw gw wkmxkv. Ckwwoik onauv "_" wqcnad!';
+    const cp = spawn("node", INPUT_TO_CLI);
+
+    let res = "";
+
+    cp.stdout.on("data", (chunk) => {
+      res += chunk.toString();
+    });
+
+    cp.stderr.on("data", (chunk) => {
+      res += chunk.toString();
+    });
+
+    cp.stdout.on("end", () => {
+      try {
+        expect(res).toBe(EXPECTED_OUTPUT);
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
+
+  test("should return correct answer fro usage example #3", (done) => {
+    const INPUT_TO_CLI = [
+      "./src/index.js",
+      "-c",
+      "A-A-A-R1-R0-R0-R0-C1-C1-A",
+      "-i",
+      "./src/input.txt",
+    ];
+    const EXPECTED_OUTPUT = 'Hvwg wg gsqfsh. Asggous opcih "_" gmapcz!';
+    const cp = spawn("node", INPUT_TO_CLI);
+
+    let res = "";
+
+    cp.stdout.on("data", (chunk) => {
+      res += chunk.toString();
+    });
+
+    cp.stderr.on("data", (chunk) => {
+      res += chunk.toString();
+    });
+
+    cp.stdout.on("end", () => {
+      try {
+        expect(res).toBe(EXPECTED_OUTPUT);
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
+
+  test("should return correct answer fro usage example #4", (done) => {
+    const INPUT_TO_CLI = [
+      "./src/index.js",
+      "-c",
+      "C1-R1-C0-C0-A-R0-R1-R1-A-C1",
+      "-i",
+      "./src/input.txt",
+    ];
+    const EXPECTED_OUTPUT = 'This is secret. Message about "_" symbol!';
+    const cp = spawn("node", INPUT_TO_CLI);
+
+    let res = "";
+
+    cp.stdout.on("data", (chunk) => {
+      res += chunk.toString();
+    });
+
+    cp.stderr.on("data", (chunk) => {
+      res += chunk.toString();
+    });
+
+    cp.stdout.on("end", () => {
+      try {
+        expect(res).toBe(EXPECTED_OUTPUT);
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
+
   test("should return error when there is no config option", (done) => {
     const INPUT_TO_CLI = ["./src/index.js", "-i", "./src/input.txt"];
     const EXPECTED_OUTPUT = "Please enter config option: -c or --config.";
